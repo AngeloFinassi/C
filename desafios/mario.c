281b1c57435fc8954d326eb2e07ebbdba6f53b45
Ângelo Matins Finassi <159829035+AngeloFinassi@users.noreply.github.com>
2025-03-29 16:35:30 -0300

    cs50 mudando programa

--- desafios/mario.c
+++ desafios/mario.c
@@ -1,46 +1,26 @@
 #include <stdio.h>
-#include <string.h>
 
-int main(){
-    int size, num_block, num_space, mem_space, mem_block;
-    
-    //Propt user ofr positive input
-    do
-    {
-        printf("\nSize (1 to 8): ");
+int main() {
+    int size;
+
+    do {
+        printf("Size (1 to 8): ");
         scanf("%d", &size);
-    } 
-    while (size < 1);
-    
-    //size-by-size grid of brick
-        mem_space = size;
-        mem_block = 1;
-        for (int i = 0; i < size; i++)
-        {
-            for (num_space = mem_space; num_space > 1; num_space--)
-            {
-                printf(" ");
-            }
-            for (num_block = 1; num_block <= mem_block; num_block++)
-            {
-                printf("#");
-            }
+    } while (size < 1 || size > 8);
 
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
             printf(" ");
-            printf(" ");
+        }
+        for (int j = 0; j < i + 1; j++) {
+            printf("#");
+        }
 
-            for (num_block = 1; num_block <= mem_block; num_block++)
-            {
-                printf("#");
-            }
-            for (num_space = mem_space; num_space > 1; num_space--)
-            {
-                printf(" ");
-            }
+        printf("  ");
 
-            printf("\n");
-            mem_space = size - (i + 1);
-            mem_block++;
+        for (int j = 0; j < i + 1; j++) {
+            printf("#");
         }
         printf("\n");
+    }
 }
